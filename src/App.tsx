eac00f530010eb1cd400857ad3d1103fa4d8a01f
Robin <Robin_Nygren@outlook.com>
2024-05-16 14:47:27 +0200

    set up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-red-600">Hejsan</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto Slab", "serif"],
+    },
+    extend: {
+      colors: {
+        "color-scheme": {
+          primary: "#0B0C10",
+          secondary: "#C5C6C7",
+          accent: "#45A29E",
+          background: "#1F2833",
+          detail: "#66FCF1",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
