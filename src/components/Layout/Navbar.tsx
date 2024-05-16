f240f53884bc9ee78c0277e37416976054912f02
samadamp <samadampankhurst@gmail.com>
2024-05-16 17:20:23 +0200

    remade navbar, removed headerfile, fixed lanague dropdown component

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,39 +1,29 @@
-import { Menu, MenuButton, MenuItems } from "@headlessui/react";
 import { NavLink } from "react-router-dom";
+import IconPrototype from  "/IconPrototype.svg"
+import Dropdown from "./Dropdown";
 
-const Navbar = () => {
+
+const Header = () => {
   return (
-    <nav className="bg-color-scheme-primary text-color-scheme-detail p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-          <Menu as="div" className="relative">
-            <MenuButton className="hover:bg-color-scheme-accent px-4 py-2 rounded flex items-center"></MenuButton>
-            <MenuItems className="absolute left-0 w-48 mt-2 origin-top-left bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-20"></MenuItems>
-          </Menu>
-          <NavLink
-            to="/my-books"
-            className="px-4 py-2 hover:bg-color-scheme-accent text-color-scheme-detail"
-          >
-            My Books
-          </NavLink>
-          <NavLink
-            to="/search"
-            className="px-4 py-2 rounded hover:bg-color-scheme-accent text-color-scheme-detail"
-          >
-            Search
-          </NavLink>
-        </div>
-        <div className="flex items-center space-x-4">
-          <input
-            type="text"
-            placeholder="Quick Search..."
-            className="p-1 text-center italic text-color-scheme-primary font-roboto-slab"
-          />
-          <button className="p-1 rounded-r-md">asd</button>
-        </div>
-      </div>
+    <nav className="bg-color-scheme-primary text-bookFlix-colors-detail">
+      
+        <NavLink
+          to="/"
+          className="text-xl font-bold text-color-scheme-detail hover:text-color-scheme-accent flex justify-between items-center"
+        >
+          <div className="bg-color-scheme-primary text-color-scheme-detail p-4">
+      <img className="w-16 ml-9" src={IconPrototype} alt="" />
+ 
+    </div>
+          <h1 className="text-3xl font-bold">
+            Borås Starter Kit
+          </h1>
+
+          <Dropdown />
+        </NavLink>
+      
     </nav>
   );
 };
 
-export default Navbar;
+export default Header;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,15 +1,15 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
-import Header from "../components/Layout/Header";
 import Navbar from "../components/Layout/Navbar";
+
 
 const Root = () => {
   /*   const location = useLocation(); */
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Header />
         <Navbar />
+  
         <main className="flex-grow bg-color-scheme-background">
           {/* {location.pathname === "/" && <placeholder />} */}
           <Outlet />
