4d924032d3ac85eba04b6ea2bd8949fd5b937597
samadamp <samadampankhurst@gmail.com>
2024-05-16 20:30:30 +0200

    stylat mainsubject

--- src/components/SubjectCard/SubjectCard.tsx
+++ src/components/SubjectCard/SubjectCard.tsx
@@ -3,11 +3,14 @@ type SubjectCardProps = { imgSrc: any; subject: string; info: string };
 export const SubjectCard = ({ imgSrc, subject, info }: SubjectCardProps) => {
   return (
     <>
-      <div className="flex flex-col">
-        <h1 className="text-2xl">{subject}</h1>
-        <img src={imgSrc} alt="" />
-        <p>{info}</p>
+    
+      <div className="flex flex-col justify-center items-center gap-5 border-color-scheme-primary border w-fit rounded-xl h-fit">
+        <h1 className="text-4xl">{subject}</h1>
+        <img src={imgSrc} alt="#" />
+        <p className="p-3 ml-5 w-72">{info}</p>
+        
       </div>
+      
     </>
   );
 };
