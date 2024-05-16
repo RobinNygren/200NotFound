d078be40751dfa49cfecee5adb588c213f1ff8e3
pjkbsn <philip.jkbsn@gmail.com>
2024-05-16 21:03:11 +0200

    added basestyling

--- src/components/SubjectDetails/SubjectDetails.tsx
+++ src/components/SubjectDetails/SubjectDetails.tsx
@@ -1,13 +1,25 @@
-type SubjectDetailsProp = {
-  title: string;
-  description: string;
-};
+import IconPrototype from "/IconPrototype.svg";
 
-export const SubjectDetails = ({ title, description }: SubjectDetailsProp) => {
+// type SubjectDetailsProp = {
+//   title: string;
+//   description: string;
+// };
+
+export const SubjectDetails = () => {
   return (
     <>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+      <div className="flex justify-between p-6 h-4/6">
+        <div className="flex items-center justify-between mb-4">
+          <img src={IconPrototype} alt="" className="mr-4" />
+        </div>
+        <div className="h-48">
+          <h1 className="text-xl font-bold mb-10">Här är titeln</h1>
+          <h2 className="text-lg mb-40">Här är description</h2>
+          <h2 className="text-lg">Här är länkarna</h2>
+        </div>
+        <div className="mb-4"></div>
+        <div></div>
+      </div>
     </>
   );
 };
--- src/routes/SubSubjects.tsx
+++ src/routes/SubSubjects.tsx
@@ -1,25 +1,26 @@
 // Här lägger vi in mer specifik info om HeadSubjects, såsom länkar och info om olika vårdcentraler, nummer till 1177 etc. Eller tex för nöjen olika sporter ifall du valt sport som headSubject.
 
-import { SubjectCard } from "../components/SubjectCard/SubjectCard";
+import { useParams } from "react-router-dom";
+import { SubjectDetails } from "../components/SubjectDetails/SubjectDetails";
 
 export const SubSubjects = () => {
-  const data = [
-    {
-      img: "insert IMG here",
-      title: "Sport",
-      description: "Här kan du spela olika sporter",
-    },
-  ];
+  // const {key} = useParams();
+  // const data = [
+  //   {
+  //     img: "insert IMG here",
+  //     title: "Sport",
+  //     description: "Här kan du spela olika sporter",
+  //   },
+  // ];
+
+  // const details = placeholderJSON.some(
+  //   (placeholder) => placeholder.key === key
+  // );
+
   return (
     <>
       <div>
-        {data.map((title) => (
-          <SubjectCard
-            imgSrc={title.img}
-            info={title.description}
-            subject={title.title}
-          />
-        ))}
+        <SubjectDetails />
       </div>
     </>
   );
