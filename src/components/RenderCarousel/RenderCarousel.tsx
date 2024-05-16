b106dd2b5d21d6f6cf6fa1f7a1b77d84e82c5deb
Robin <Robin_Nygren@outlook.com>
2024-05-16 17:35:25 +0200

    added pictures

--- src/components/RenderCarousel/RenderCarousel.tsx
+++ src/components/RenderCarousel/RenderCarousel.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
 import Card from "../Card/Card";
+import { RenderCarouselProps } from "../../types/types";
 
-const RenderCarousel: React.FC = () => {
+const RenderCarousel: React.FC<RenderCarouselProps> = ({ images }) => {
   return (
     <div>
       <Carousel title="subject">
-        <Card />
+        {images.map((image, index) => (
+          <Card key={index} src={image.src} alt={image.alt} />
+        ))}
       </Carousel>
     </div>
   );
