d1eccc3a9ad3347d1cae3e4414d2365421282c89
pjkbsn <philip.jkbsn@gmail.com>
2024-05-16 19:50:01 +0200

    changed

--- src/components/SubjectCard/SubjectCard.tsx
+++ src/components/SubjectCard/SubjectCard.tsx
@@ -1,11 +1,11 @@
-type SubjectCardProps = { imgSrc: any; subject: any; info: string };
+type SubjectCardProps = { imgSrc: any; subject: string; info: string };
 
 export const SubjectCard = ({ imgSrc, subject, info }: SubjectCardProps) => {
   return (
     <>
-      <div className="flex">
+      <div>
         <h1>{subject}</h1>
-        <img src={imgSrc} alt={imgSrc} />
+        <img src={imgSrc} alt="" />
         <p>{info}</p>
       </div>
     </>
